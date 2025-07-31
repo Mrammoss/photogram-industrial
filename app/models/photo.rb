e6e4bf35613f841d67878996b6561e63ca2fb085
Miguel Ramos <142689773+Mrammoss@users.noreply.github.com>
2025-07-31 00:47:06 +0000

    Added username to sign up form

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -20,20 +20,19 @@
 #  fk_rails_...  (owner_id => users.id)
 #
 class Photo < ApplicationRecord
-
-mount_uploader :image, ImageUploader
+  mount_uploader :image, ImageUploader
 
   belongs_to :owner, class_name: "User", counter_cache: true
   has_many :comments
-  
+
   has_many :likes
   has_many :fans, through: :likes
 
   validates :caption, presence: true
   validates :image, presence: true
 
-  scope :past_week, -> { where(created_at: 1.week.ago...)}
-  scope :by_likes, -> { order(likes_count: :desc)}
+  scope :past_week, -> { where(created_at: 1.week.ago...) }
+  scope :by_likes, -> { order(likes_count: :desc) }
 
-  scope :latest, -> { order(created_at: :desc)}
+  scope :latest, -> { order(created_at: :desc) }
 end
